09751a9e89d324ad80dc3831479b189dbba1d2cd
Grzegorz Pietrusza <grzegorz@fedora.home>
2014-10-18 13:14:31 +0200

    v1

---
+++ ascii_to_string/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+
+	string ascii; //temporary for each exploded item
+	ostringstream tekst; //output string stream
+	string user_input; //user input
+	
+	cout << "Podaj kod ASCII:\n";
+	getline(cin,user_input);
+	stringstream ss(user_input);
+
+	while(getline(ss,ascii,' ')) {
+		tekst << (char)atoi(ascii.c_str());
+
+	}
+	
+	cout << "Tekst:\n" << tekst.str() << endl;
+	
+	return 0;
+}
